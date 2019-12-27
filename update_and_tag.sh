965114bd2df0d2965d647da2548719c83df78685
jwlodek <jwlodek@cs.stonybrook.edu>
2019-12-27 11:22:56 -0500

    Working on script

--- update_and_tag.sh
+++ update_and_tag.sh
@@ -2,7 +2,7 @@
 
 echo "Grabbing most recent ADCore tag version..."
 # Need to add command to get most recent ADCore tag
-#ADCORE_VERSION=$(git ls-remote )
+ADCORE_VERSION=($(git -c http.proxy=http://proxy:8888 ls-remote --tags https://github.com/areaDetector/ADCore | awk -F/ '{ print $3 }' | tac))
 
 echo "Grabbing installSynApps..."
 git clone https://github.com/epicsNSLS2-deploy/installSynApps
@@ -14,9 +14,18 @@ echo "Updating version numbers. This May take a while..."
 for d in ../*/ ; do
   if [ "$d" != "installSynApps" ];
   then
-    python3 -u installCLI.py -c ../$d -v
+    echo
+    python3 -u installCLI.py -c $d -v
   fi
 done
+
+cd ..
+rm -rf installSynApps
+
+read -e -p "Would you like to tag, commit, and push updated tags? (y/n) > " UPDATE
+
+if [ "$UPDATE" == "y" ];
+then
 
 echo "Finished updating version numbers. Creating Install-Configurations tag..."
 git add -A
@@ -26,3 +35,9 @@ echo "Pushing changes and new tag."
 git push origin master
 git push origin --tags
 echo "Done. New tag version: $ADCORE_VERSION."
+
+else
+
+echo "Tags updated, done."
+
+fi
