fd694499993a4ccd4330834456cc293ddc4ae67b
Jakub Wlodek <jwlodek.dev@gmail.com>
2020-10-21 16:09:39 -0400

    Fix Typo

--- misc/usbptz/customBuildScripts/FFMPEGSERVER.sh
+++ misc/usbptz/customBuildScripts/FFMPEGSERVER.sh
@@ -3,7 +3,7 @@ rm -rf vendor/yasm*
 FFMPEG="https://ffmpeg.org/releases/ffmpeg-4.0.2.tar.xz"
 YASM="http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz"
 
-for z in $SOURCE $YASM; do
+for z in $FFMPEG $YASM; do
     wget -P vendor $z
 done
 
